5c3b41862c79531c61f32fbe60cee78933fc0bd0
Mohammed Alhalwachi <Mohammed.abdameeralhalwachi@gmail.com>
2021-05-10 15:07:45 +0300

    add support for changing the url, and delete files and folder after terminating the CLI

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,17 @@
 // process.on('unhandledRejection', function (err) {
-//     console.error(err);
+//     console.trace();
+//     console.error(chalk`{red Something went wrong}`);
+//     process.exit();
 // });
 require = require('esm')(module, /*, options */);
 const querystring = require('querystring');
 const Odoo = require('odoo-await');
 const fs = require('fs').promises;
-const util = require('util');
+const rmdirSync = require('fs').rmdirSync;
 const path = require('path');
 const mkdirp = require('mkdirp');
-const readline = require('readline');
 const chokidar = require('chokidar');
-const {Form, Select, prompt} = require('enquirer');
+const { Select, prompt } = require('enquirer');
 const arg = require('arg');
 import chalk from 'chalk';
 
@@ -31,7 +32,7 @@ const getArgs = () => {
         // Aliases
         '-y': '--yes',
     });
-    
+
     return {
         skipPrompt: args['--yes'],
     }
@@ -74,72 +75,37 @@ const promptConnectionDetails = async () => {
 }
 
 const odooConnect = async (baseUrl, port, db, username, password) => {
-    const odoo = new Odoo({
-        baseUrl: baseUrl,
-        port: port,
-        db: db,
-        username: username,
-        password: password
-    });
-
-    await odoo.connect();
-    return odoo;
-}
-
-export async function cli() {
-    // const username = 'admin';
-    // const password = 'admin';
-    // const db = 'odoo';
-
-    const args = getArgs();
-
-    // console.log(args);
-    // process.exit();
-
-    let {baseUrl, port, db, username, password} = {
-        baseUrl: 'http://localhost',
-        port: 8069,
-        db: 'odoo',
-        username: 'admin',
-        password: 'admin',
-    };
-
-    if (!args.skipPrompt) {
-        const connectionDetails = await promptConnectionDetails();
-        baseUrl = connectionDetails.baseUrl;
-        port = connectionDetails.port;
-        db = connectionDetails.db;
-        username = connectionDetails.username;
-        password = connectionDetails.password;
-    }
-
-    // console.log({baseUrl, port, db, username, password});
-
-    // Connect to Odoo
-    let odoo;
     try {
-        odoo = await odooConnect(baseUrl, port, db, username, password);
+        const odoo = new Odoo({
+            baseUrl: baseUrl,
+            port: port,
+            db: db,
+            username: username,
+            password: password
+        });
+
+        await odoo.connect();
         console.log(chalk`{green Connected to Odoo:} \n \turl = {blue ${baseUrl}}, port = {blue ${port}} \n \tdb = {blue ${db}}, username = {blue ${username}}, password = {blue ${password}}`);
         console.log(); // prints new line
+
+        return odoo;
     } catch (error) {
         console.error(error);
         console.error(chalk`{red Failed to connect to Odoo with:} \n \turl = {blue ${baseUrl}}, port = {blue ${port}} \n \tdb = {blue ${db}}, username = {blue ${username}}, password = {blue ${password}} \n{red Please check the entered information}`);
         process.exit(1);
     }
-    // url = "http://localhost:8069/web#id=72&action=28&model=ir.ui.view&view_type=form&cids=&menu_id=4";
+}
 
-    // console.log(url);
-    // process.exit();
-
+const promptRecordUrl = async () => {
     let url;
     let fieldName;
-    let record;
     let options;
     try {
         url = await prompt({
             type: 'input',
             name: 'url',
-            message: 'Record url:'
+            message: 'Record url:',
+            initial: 'http://localhost:8069/web#id=288&action=28&model=ir.ui.view&view_type=form&cids=&menu_id=4', // FIXME:: remove. this is only for testing
         }).then(res => res.url);
         // validate and get the options out of the url
         let urlObj = new URL(url); // this also validate the url
@@ -151,23 +117,20 @@ export async function cli() {
             message: 'Field name:',
             initial: 'arch_base'
         }).then(res => res.field);
-        
-        record = await odoo.read(options.model, Number.parseInt(options.id)).then(records => records[0]);
+
     } catch (error) {
         console.error(chalk`{red Failed to read the record:} \n \turl = {blue ${url}} \n{red Please check the provided information}`);
         process.exit(1);
     }
-    const field = record[fieldName];
-    
-    // console.log(record);
-    if (field === undefined) {
-        console.error(chalk`{red There is no "${fieldName}" on the record} \n{red Please check the provided information}`);
-        process.exit(1);
-    }
-    console.log(chalk`\n{green Connected to the record}\n`);
 
-    // console.log(record.arch_base);
-    
+    return {
+        url,
+        fieldName,
+        options
+    }
+}
+
+async function createWatcher(record, fieldName, odoo, options) {
     let folderPath;
     let fileExt;
     let filePath;
@@ -195,35 +158,92 @@ export async function cli() {
         await mkdirp(folderPath);
         await fs.writeFile(filePath, record[fieldName]);
 
+        process.on('exit', () => {
+            // delete edit_files folder and its contents
+            rmdirSync(folderPath, { recursive: true });
+            process.exit();
+        });
+
         console.log(chalk`{green File created and watched at} {blue ${filePath}}\n`);
+        console.log(chalk`{blue To change the watched record press 'ch'}\n`);
     } catch (error) {
         console.error(error);
         console.error(chalk`{red An error happened while creating the file}`);
         process.exit();
     }
 
-    // console.log(filePath);
-    // process.exit();
-
-    chokidar.watch(filePath).on('change', async (event, path) => {
+    return chokidar.watch(filePath).on('change', async (event, path) => {
         const file = await fs.readFile(filePath).then(file => file.toString());
-        // console.log(file.toString());
-        // process.exit();
-        
         try {
             const isSuccessful = await odoo.update(options.model, Number.parseInt(options.id), {
                 [fieldName]: file,
             });
-            if (isSuccessful) {
-                console.log(chalk`{blue Record updated}`);
-            } else {
-                console.error(chalk`{red Failed to update the record}`);
-            }
+            if (!isSuccessful) throw new Error('Updating the record was not successful');
+            console.log(chalk`{blue Record updated}`);
         } catch (error) {
             console.error(error);
             console.error(chalk`{red Failed to update the record. Check the error above.}`);
         }
     });
+}
+
+async function watchFile(odoo) {
+    const { url, fieldName, options } = await promptRecordUrl();
+    const record = await odoo.read(options.model, Number.parseInt(options.id)).then(records => records[0]);
+    const field = record[fieldName];
+
+    if (field === undefined) {
+        console.error(chalk`{red There is no "${fieldName}" on the record} \n{red Please check the provided information}`);
+        process.exit(1);
+    }
+    console.log(chalk`{green Connected to the record}\n`);
+
+    let watcher = await createWatcher(record, fieldName, odoo, options);
+    return { fieldName, options, record, watcher };
+}
+
+export async function cli() {
+    try {
+        const args = getArgs();
+
+        // console.log(args);
+        // process.exit();
+
+        let { baseUrl, port, db, username, password } = {
+            baseUrl: 'http://localhost',
+            port: 8069,
+            db: 'odoo',
+            username: 'admin',
+            password: 'admin',
+        };
+
+        if (!args.skipPrompt) {
+            const connectionDetails = await promptConnectionDetails();
+            baseUrl = connectionDetails.baseUrl;
+            port = connectionDetails.port;
+            db = connectionDetails.db;
+            username = connectionDetails.username;
+            password = connectionDetails.password;
+        }
+
+        // Connect to Odoo
+        const odoo = await odooConnect(baseUrl, port, db, username, password);
+
+        let { fieldName, options, record, watcher } = await watchFile(odoo);
+
+        process.stdin.setEncoding('utf8');
+        process.stdin.resume();
+        process.stdin.on("data", async (data) => {
+            const str = data.toString().trim().toLowerCase();
+            if (str === 'ch') {
+                await watcher.close();
+                watcher = await watchFile(odoo);
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        console.error(chalk`{red Something went wrong!}`);
+    }
 }
 
 // cli();
